e461500d4bb2a9f832efa1914498f8a847e9ae4e
Pedro Leandro <pedro.healthdev@gmail.com>
2024-11-13 08:51:32 -0300

    feat: all variable fields possible added

--- variables.tf
+++ variables.tf
@@ -1,8 +1,12 @@
-# The variables file may have any name
-# Since all .tf files are loaded anyway
-
 variable "container_name" {
     description = "Value of the name for the Docker container"
     type = string
     default = "defaultContainerName"
+    sensitive = false
+    nullable = false
+
+    validation {
+      condition = length(var.container_name) > 4
+      error_message = "The container name must be more than 4 characters wide"
+    }
 }
